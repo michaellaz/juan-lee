8d806317709ee35e016f2c3388aa1ad9a8bd2636
ad <ad@laptopad>
2020-07-26 12:47:47 -0500

    Add share option

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const shareScreen = document.querySelector("#share-screen");
+shareScreen.style.display = "none";
+document.querySelector("#share-button").addEventListener("click", () => {
+    if (shareScreen.style.display === "none") {
+        shareScreen.style.display = "block"; 
+    } else {
+        shareScreen.style.display = "none"; 
+    }
+});
+
+
+document.querySelector("#copy-to-clipboard").addEventListener("click", () => {
+    if (!navigator.clipboard) {
+        return; 
+    }
+
+    navigator.clipboard.writeText("https://michaellaz.github.io/juan-lee/")
+        .then(() => alert("Copied URL to clipboard."))
+        .catch(() => alert("Failed to copy URL to clipboard."));
+});
