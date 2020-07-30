1fed03fcab683a55f3d229450378c8c657d809c8
ad <ad@laptopad>
2020-07-30 14:30:16 -0500

    Update copy-to-clipboard feature

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,33 @@ document.querySelector("#copy-to-clipboard").addEventListener("click", () => {
         return; 
     }
 
+    // navigator.clipboard.writeText("https://michaellaz.netlify.app")
+    //     .then(() => alert("Copied URL to clipboard."))
+    //     .catch(() => alert("Failed to copy URL to clipboard."));
+
     navigator.clipboard.writeText("https://michaellaz.netlify.app")
-        .then(() => alert("Copied URL to clipboard."))
-        .catch(() => alert("Failed to copy URL to clipboard."));
+        .then((async () => {
+            const copiedFloater = document.createElement("div");
+            document.body.appendChild(copiedFloater);
+
+            copiedFloater.innerHTML = "Copied URL";
+
+            copiedFloater.style.position = "absolute";
+            copiedFloater.style.top = "50%";
+            copiedFloater.style.left = "50%";
+            copiedFloater.style.transform = "translate(-50%, 0)";
+            copiedFloater.style.background = "black";
+            copiedFloater.style.color = "white";
+            copiedFloater.style.borderRadius = "5px";
+            copiedFloater.style.padding = "10px";
+            copiedFloater.style.opacity = 0.75;
+
+            await sleep(1000);
+
+            document.body.removeChild(copiedFloater);
+        })());
 });
+
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
